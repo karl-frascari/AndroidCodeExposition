1621b3839e0ea223dabca2abe27589776106e5c4
karlFrascari2 <karl.frascari@gmail.com>
2016-05-16 22:16:11 -0300

    Activities

---
+++ AndroidCodeExposition/app/src/main/java/karl/androidcodeexposition/ProcessLoginActivity.java
@@ -0,0 +1,12 @@
+package karl.androidcodeexposition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProcessLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
